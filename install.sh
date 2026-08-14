ea70cd65c3480244b383f2ec185a592a957ff4f9
geor0001 <19818909512@163.com>
2026-08-14 12:35:53 +0800

    Add cloning commands for Yuanbao projects
    Added git clone commands for Yuanbao projects.

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 echo yuanbao_quickinstaller(git)v1.5
 echo by geor
 echo 准备克隆待选择的所有项目
+git clone https://github.com/geor0001/Yuanbao-V6.5-G.git
+echo 完成1/2
+git clone github.com/anxi78/yuanbao_bot_client
+echo 完成2/2
+echo 进行下一步
